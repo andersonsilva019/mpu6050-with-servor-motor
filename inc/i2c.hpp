0c43ba20a6d9982249215bc8114d80a7865420e6
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2022-12-01 00:23:58 -0300

    +Organização dos parâmetros+ e remoção de um comentário

--- inc/i2c.hpp
+++ inc/i2c.hpp
@@ -22,12 +22,10 @@ public:
     I2C(std::string bus, uint8_t deviceAddress);
     void writeI2C(uint8_t registerAddress, uint8_t data);
     void writeBitI2C(uint8_t registerAddress, uint8_t data, uint8_t bitNum);
-    // TO IMPLEMENT
     void writeBitsI2C(uint8_t registerAddress, uint8_t data, uint8_t length, uint8_t startBit);
 
     void readI2C(uint8_t registerAddress, uint8_t* data, uint8_t length);
     uint8_t readBitI2C(uint8_t registerAddress, uint8_t bitNum);
-    // TO IMPLEMENT
     uint8_t readBitsI2C(uint8_t registerAddress, uint8_t length, uint8_t startBit);
     uint8_t readByteI2C(uint8_t registerAddress);
     uint16_t readWordI2C(uint8_t registerAddress);
